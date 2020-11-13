084fcc0f341af5134f2e74db4b26ef07ef5993b2
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2020-11-13 23:25:17 +0530

    feat: Initialize empty project

---
+++ bin/translate_ref_generator.dart
@@ -0,0 +1,5 @@
+import 'package:translate_ref_generator/translate_ref_generator.dart' as translate_ref_generator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${translate_ref_generator.calculate()}!');
+}
---
+++ test/translate_ref_generator_test.dart
@@ -0,0 +1,8 @@
+import 'package:translate_ref_generator/translate_ref_generator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
